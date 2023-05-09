e9ab9a0158e1f58f5a25c63847b9aefe79dfef86
Manas12347 <manas.yaashi@gmail.com>
2023-05-09 13:29:44 +0530

    first commit

---
+++ Snow.js
@@ -0,0 +1,47 @@
+class Snow{
+	constructor(x,y,)
+	{
+		var options={
+			'restitution':1.3,
+            'friction':0.1,
+            'density':1.0
+           
+		}
+		this.x=x;
+		this.y=y;
+		this.body=Bodies.circle(this.x, this.y, options)
+
+		this.image = loadImage("snow.png");
+        World.add(world, this.body)
+
+	}
+	mouseDragged(){
+		
+
+			Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+		
+	}
+	display()
+	{
+			var snowpos=this.body.position;		
+			push()
+			translate(snowpos.x, snowpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			//stroke("black");
+			fill(rgb(92, 114, 237));
+            
+			imageMode(CENTER);
+			image(this.image, 0, 0, this.width, this.height);
+	
+		   
+
+            pop()
+	}
+
+
+	
+
+    
+
+}
